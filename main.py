a93e7675fe29e257498135b7dade2569684c18e6
Daniel Gonçalves <bgondaniel@gmail.com>
2024-04-09 19:49:08 +0200

    Added word and character count function

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 def main():
     book_path = "books/frankenstein.txt"
     book_content = read_book(book_path)
-    print(book_content)
+    word_count = count_words(book_content)
+    characters_count = count_characters(book_content)
+    print(characters_count)
 
 def read_book(book_path):
     with open(book_path) as f:
         return f.read()
+    
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def count_characters(text):
+    letter_count = {}
+    lower_case_text = text.lower()
+    for character in lower_case_text:
+        if character in letter_count:
+            letter_count[character] += 1
+        else:
+            letter_count[character] = 1
+
+    return letter_count
 
 main()
