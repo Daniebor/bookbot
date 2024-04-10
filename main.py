3c2efedbca925b69609e2473468d01bcc79a558a
Daniel Gonçalves <bgondaniel@gmail.com>
2024-04-10 11:33:47 +0200

    Add report functionality

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     book_content = read_book(book_path)
     word_count = count_words(book_content)
     characters_count = count_characters(book_content)
-    print(characters_count)
+    print_report(book_path, word_count, characters_count)
 
 def read_book(book_path):
     with open(book_path) as f:
@@ -23,5 +23,29 @@ def count_characters(text):
             letter_count[character] = 1
 
     return letter_count
+
+def print_report(book, word_count, character_count):
+    print(f"--- Begin report of {book} ---")
+    print(f"{word_count} words found in the document")
+
+    character_count_sorted = convert_dic_to_list(character_count)
+    character_count_sorted.sort(reverse=True, key=sort_on)
+
+    for character in character_count_sorted:
+        if character["character"].isalpha():
+            print(f"the '{character["character"]}' character was found {character["count"]} times")
+            
+    print("--- End of report ---")
+
+
+def convert_dic_to_list(dictionary):
+    dic_list = []
+    for item in dictionary:
+        dic_list.append({"character": item, "count": dictionary[item]})
+    return dic_list
+
+def sort_on(dict):
+    return dict["count"]
+
 
 main()
